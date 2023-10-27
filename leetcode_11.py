420f39ab34a6a8dd989da986ee427aa85718cf31
Cherifi-Houdaifa <houdaifacherifi618@gmail.com>
2023-10-27 18:52:24 +0100

    Solved leetcode problem 26 "Container with most water" with only two hints

--- leetcode_11.py
+++ leetcode_11.py
@@ -1,8 +1,24 @@
 def main(height: list[int]):
-    print("\rhi", end="")
-    print("\rhi", end="")
+    left = 0
+    right = len(height) - 1
+    maxArea = -1
+
+    while left < len(height) and right > 0:
+        # print(right, left)
+        # print("--------------", min(height[left],height[right]) * abs(right - left))
+
+        if min(height[left],height[right]) * abs(right - left) > maxArea:
+            maxArea = min(height[left],height[right]) * abs(right - left)
+        
+        if height[left] > height[right]:
+            right -=1
+        elif height[left] < height[right]:
+            left +=1
+        else:
+            left +=1
+        
+    return maxArea
 
 
 
-
-print(main([1,8,6,2,5,4,8,3,7]))
+print(main([1,1]))
